b6c2ec73db9b1bf39da25cb3929ed80e11b1dea9
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-01-22 11:14:26 +0000

    feat: implement and register elasticsearch extensions

--- src/DotnetELK/Program.cs
+++ src/DotnetELK/Program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using DotnetELK.Extensionss;
 using Serilog;
 using Serilog.Exceptions;
 using Serilog.Sinks.Elasticsearch;
@@ -14,6 +15,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddElasticSearch(builder.Configuration);
 
 var app = builder.Build();
 
